26c636dd4e80a664588261110296a75b08ea7adc
NickGittings <38570076+NickGittings@users.noreply.github.com>
2024-08-16 16:36:04 -0400

    Added bootstrap list group

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import Message from './message';
+import logo from "./logo.svg";
+import "./App.css";
+import Message from "./message";
+import ListGroup from "./components/ListGroup";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return <div><Message></Message></div>
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App;
