1b13b87452fa1e778862a0f25892a4ec442c5725
NickGittings <38570076+NickGittings@users.noreply.github.com>
2024-08-16 16:51:23 -0400

    Added conditonal for empty list.

--- src/components/ListGroup.js
+++ src/components/ListGroup.js
@@ -1,17 +1,19 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  let items = ["New York", "Chicago", "Louisville", "Indianpolis"];
+  items = [];
+
+  const emptyMessage = items.length === 0 && <p>No items found</p>;
+
   return (
     <Fragment>
       <h1>List</h1>
+      {emptyMessage}
       <ul className="list-group">
-        <li className="list-group-item active" aria-current="true">
-          An active item
-        </li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </Fragment>
   );
